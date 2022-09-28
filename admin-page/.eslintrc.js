4b88460ae53d7781d622dd644c19c0be543b048e
devKya <kihaok@gmail.com>
2022-09-28 17:19:56 +0900

    install Vuetify

--- admin-page/.eslintrc.js
+++ admin-page/.eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    parser: "@babel/eslint-parser",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    'prettier/prettier': [
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		'plugin:vue/essential',
+		'eslint:recommended',
+		'plugin:prettier/recommended',
+	],
+	parserOptions: {
+		parser: '@babel/eslint-parser',
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'prettier/prettier': [
 			'error',
 			{
 				singleQuote: true,
@@ -28,16 +28,17 @@ module.exports = {
 				endOfLine: 'auto',
 			},
 		],
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
-      env: {
-        jest: true,
-      },
-    },
-  ],
+		'vue/multi-word-component-names': 0,
+	},
+	overrides: [
+		{
+			files: [
+				'**/__tests__/*.{j,t}s?(x)',
+				'**/tests/unit/**/*.spec.{j,t}s?(x)',
+			],
+			env: {
+				jest: true,
+			},
+		},
+	],
 };
